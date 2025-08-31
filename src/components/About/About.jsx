25bff06d47f0125dd370c99e3ff2ff953e76b463
Subodh Gangwar <subodhgangwar0123@gmail.com>
2025-08-31 17:53:12 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -53,7 +53,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1gSo3YitgWrKQ1cmBTAuzq-qAE7cNBDk2/view?usp=sharing"
+            href="https://drive.google.com/file/d/1RR4OrzfuckbrMcDXOT0yKUFcAbwl5pPG/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
